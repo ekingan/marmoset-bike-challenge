bba19603945fbaee7bc25d43c64bc8edd3d9bdd1
ekingan <ekingan@gmail.com>
2017-04-28 12:34:10 -0700

    Reformated for small screen, added delete

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,6 +1,7 @@
 class Ride < ApplicationRecord
   belongs_to :user
   after_save :calculate_user_score
+  after_destroy :calculate_user_score
 
   def calculate_user_score
     score = user.rides.count
